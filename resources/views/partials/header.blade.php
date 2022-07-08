294f73f87bc9fa75d760e416a42c1bc5e04b5c11
urlastway <andriy.semenchenko13@gmail.com>
2022-07-08 10:20:34 +0200

    aggiunta header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,25 @@
 <header>
-    <h1>DC Comics</h1>
+    <div class="header__top">
+        <div class="left_text">
+            <p>DC POWER VISA</p>
+        </div>
+        <div class="right_text">
+            <p>ADDITIONAL DC SITES</p>
+        </div>
+    </div>
+    <div class="menu">
+        <img src="{{asset('img/dc-logo.png')}}" alt="Logo DC Comics">
+        <ul>
+            <li><a href="#">Characters</li>
+            <li><a href="#">Comics</li>
+            <li><a href="#">Muvies</li>
+            <li><a href="#">TV</li>
+            <li><a href="#">Games</li>
+            <li><a href="#">Colaborations</li>
+            <li><a href="#">Characters</li>
+            <li><a href="#">Fans</li>
+            <li><a href="#">News</li>
+            <li><a href="#">Shop</li>
+        </ul>
+    </div>
 </header>
