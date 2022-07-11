8a629cbdbc194bdb80c6d96c7d1d507c11681f31
urlastway <andriy.semenchenko13@gmail.com>
2022-07-11 15:30:53 +0200

    finished

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,5 +5,21 @@
 @endsection
 
 @section('page-content')
-    <h1>{{$prodotto['title']}}</h1>
+<div class="page__content" style="width: 100%; display: flex; justify-content: center;">
+    <div class="container">
+        <img src="{{$prodotto['thumb']}}" alt="{{$prodotto['title']}}">
+        <h1>{{$prodotto['title']}}</h1>
+        <div class="price__table">
+            <h4>Price: {{$prodotto['price']}}</h4>
+        </div>
+
+        <p>{{$prodotto['description']}}</p>
+        <div class="specs" style="padding-top: 20px;">
+            <h3>Specs:</h3>
+            <p>{{$prodotto['sale_date']}}</p>
+            <p>{{$prodotto['type']}}</p>
+            <p>{{$prodotto['price']}}</p>
+        </div>
+    </div>
+</div>
 @endsection
